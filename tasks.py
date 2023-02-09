43a14c51266c94961e4f16c87a4d69f8b536c36c
ipcampbell12 <ipcampbell12@gmail.com>
2023-02-09 09:23:36 -0800

    templates

--- tasks.py
+++ tasks.py
@@ -1,13 +1,19 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 domain = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
 
 
-def send_simple_message(to, subject, body):
+def send_simple_message(to, subject, body, html):
     
     return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
@@ -15,11 +21,15 @@ def send_simple_message(to, subject, body):
 		data={"from": f"Ian Campbell <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
-			"text": body})
+			"text": body,
+            "html":html})
 
 def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Sucessfully signed up",
-        f"Hi {username}! You have successfully signed up to teh Stores REST API!"
+        f"Hi {username}! You have successfully signed up to teh Stores REST API!",
+        render_template("email/action.html", username=username)
     )
+
+    ## Jinja, looking at context, 
